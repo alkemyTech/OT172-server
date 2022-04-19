8331ed28c3d30dc4a99bc0800bac57618a82a891
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2022-04-19 18:22:34 -0300

    Creation of migration and models of activities

---
+++ models/activities.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Activities extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Activities.init({
+    name: DataTypes.STRING,
+    image: DataTypes.BLOB,
+    content: DataTypes.STRING,
+    deletedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Activities',
+  });
+  return Activities;
+};
