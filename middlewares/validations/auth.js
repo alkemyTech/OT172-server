97d78d7c50dff0383e8a308028ba7dca98bcec99
pasini_mariano <pasinimariano@gmail.com>
2022-04-20 11:11:44 -0300

    It is not necessary to send the name for token creation, the token verification middleware was added in the corresponding folder

--- middlewares/validations/auth.js
+++ middlewares/validations/auth.js
@@ -1,19 +1,39 @@
-const Joi = require('joi');
+require('dotenv').config
+const jwt = require('jsonwebtoken')
+const Joi = require('joi')
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET
 
 const authValidation = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required()
-});
+  email: Joi.string()
+    .email()
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .required()
+})
 
-const validateLogin = (req,res,next) => {
-    const result = authValidation.validate(req.body);
-    if(!result.error){
-        next();
-    } else {
-        console.log(`Authentication have errors: [${result.error.message}]`);
-        res.status(422).json(result.error.message);
-    }
+const validateToken = (req, res, next) => {
+  const Token = req.headers['x-access-token']
+
+  if (!Token) return res.status(403).send('Token is missing')
+
+  try {
+    jwt.verify(Token, TOKEN_SECRET)
+    next()
+  } catch (error) {
+    return res.status(401).send('Token is invalid')
+  }
 }
 
+const validateLogin = (req, res, next) => {
+  const result = authValidation.validate(req.body)
+  if (!result.error) {
+    next()
+  } else {
+    console.log(`Authentication have errors: [${result.error.message}]`)
+    res.status(422).json(result.error.message)
+  }
+}
 
-module.exports = { validateLogin }
+module.exports = { validateToken, validateLogin }
