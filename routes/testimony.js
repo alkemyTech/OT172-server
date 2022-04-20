4f8f36e6005749e2a0c2310ee31f9e3827ee8ac3
nhoga <n9746ab@gmail.com>
2022-04-20 20:18:21 -0300

    add route post testimony with middleware and controller

--- routes/testimony.js
+++ routes/testimony.js
@@ -1,1 +1,14 @@
-//ok
+var express = require("express");
+var router = express.Router();
+const {
+  validatedTestimony,
+} = require("../middlewares/validations/TestimonyValidations");
+const postTestimonyController = require("../controllers/testimonyController");
+
+router.get("/", function (req, res, next) {
+  res.send("respond with a testimonial");
+});
+
+router.post("/", [validatedTestimony], postTestimonyController.postTestimony);
+
+module.exports = router;
