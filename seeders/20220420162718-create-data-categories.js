7d2d1ecbba843e92852388b0d972fc21403a08fe
fom78 <fom78a@gmail.com>
2022-04-21 13:41:18 -0300

    fix migartions and seeds orders

---
+++ seeders/20220420162718-create-data-categories.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "Categories",
+      [
+        {
+          name: "novedad",
+          description: "novedad de la ong",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "nueva novedad",
+          description: "reciente novedad de la ong",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
