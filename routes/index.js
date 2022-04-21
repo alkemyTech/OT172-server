4cc33a138f21c7bb34bf006d39d184c9d3edf9c4
Fernando Masino <49108644+fom78@users.noreply.github.com>
2022-04-21 15:03:34 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const authRouter = require('./auth');
 const usersAuth = require('./users');
 const categoriesRouter = require('./categories')
+const activitiesRouter = require('./activities');
+const testimonyRouter = require("./testimony");
 
 router.use('/auth', authRouter);
 router.use('/users', usersAuth);
-router.use('/users', usersAuth);
 router.use('/categories', categoriesRouter)
+app.use('/testimonials', testimonyRouter);
+app.use('/activities', activitiesRouter);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
