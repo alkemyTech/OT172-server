24d694c8e3b4946221e198f3d95b1cd40431dcab
KarlitosD <37454843+KarlitosD@users.noreply.github.com>
2022-04-21 14:44:16 -0300

    Create model, migration and seeder for Contacts (#16)

---
+++ models/contacts.js
@@ -0,0 +1,23 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Contacts extends Model {
+    static associate(models) {
+      //TODO: add associations
+    }
+  }
+  Contacts.init({
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        phone: DataTypes.STRING,
+        message: DataTypes.STRING,
+        deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Contacts",
+      paranoid: true
+    }
+  );
+  return Contacts;
+};
