010d095675027638f383b973b023e8bb2c3a2766
fom78 <fom78a@gmail.com>
2022-04-21 12:03:41 -0300

    delete views and ejs dependence

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ require('dotenv').config()
 const app = express()
 app.use(cors())
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
