584737d629b7ba2af2d71e547041c45004bc7384
fom78 <fom78a@gmail.com>
2022-04-22 11:04:47 -0300

    add migrations and seed  for organization

---
+++ migrations/20220418134954-create-organizations.js
@@ -0,0 +1,50 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Organizations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      welcomeText: {
+        type: Sequelize.STRING
+      },
+      urlFacebook: {
+        type: Sequelize.STRING
+      },
+      urlLinkedin: {
+        type: Sequelize.STRING
+      },
+      urlInstagram: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Organizations')
+  }
+}
