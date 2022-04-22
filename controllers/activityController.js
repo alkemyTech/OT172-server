a7573de095fef63294a60fe593d1bbaea04da978
Nicolás Alexis Borzone <n9746ab@gmail.com>
2022-04-22 14:13:18 -0300

    Actividades 67 (#21)
    * add updateActivity function
    
    * seeder and put route for activities
    
    * fix activities image model type and migration, add image to validation, and refactor create activity function
    
    Co-authored-by: Fernando Masino <49108644+fom78@users.noreply.github.com>

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,23 +1,41 @@
-const { builCategory } = require('../services/activityService')
+
+const { Activities } = require("../models");
+
+const createActivity = async (req, res) => {
+  try {
+    const { name, image, content } = req.body;
+    const create = await Activities.create({
+      name,
+      image,
+      content,
+    });
+    if (!create) {
+      return res.status(402).json({
+        created: false,
+        msg: "Something got wrong while creating the activity.",
+      });
+    }
+    res.status(200).json({
+      created: true,
+      msg: "Activity created successfully!",
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const updateActivity = async (req, res) => {
+  let id = req.params.id;
+  try {
+    const activity = await Activities.update(req.body, { where: { id: id } });
+    res.status(200).send({ id: activity, message: "updated" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
-  async createActivity (req, res) {
-    try {
-      const create = builCategory(req.body)
+  createActivity,
+  updateActivity,
+};
 
-      if (!create) {
-        return res.status(402).json({
-          ok: false,
-          msg: 'Something got wrong while creating the activity.'
-        })
-      }
-
-      res.status(200).json({
-        ok: true,
-        msg: 'Activity created successfully!'
-      })
-    } catch (err) {
-      res.status(500).json({ error: err.message })
-    }
-  }
-}
