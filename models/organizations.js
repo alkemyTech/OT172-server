8b8ca08b9fb11d67f7d9323bf1d389ae170f99bb
fom78 <fom78a@gmail.com>
2022-04-22 11:04:28 -0300

    add models for organization

---
+++ models/organizations.js
@@ -0,0 +1,28 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Organizations extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Organizations.init({
+    name: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    address: DataTypes.STRING,
+    welcomeText: DataTypes.STRING,
+    urlFacebook: DataTypes.STRING,
+    urlLinkedin: DataTypes.STRING,
+    urlInstagram: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Organizations'
+  })
+  return Organizations
+}
