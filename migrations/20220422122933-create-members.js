e028eb30f4cdb7b6b620fadac6edfd281a2beab1
pasini_mariano <pasinimariano@gmail.com>
2022-04-22 13:13:12 -0300

    Miembros 88 (#24)
    * The controllers were unified, and services were created for each of them
    
    * The members model was created

---
+++ migrations/20220422122933-create-members.js
@@ -0,0 +1,31 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Members', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.BLOB
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Members')
+  }
+}
