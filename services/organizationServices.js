749938cea505e0c8c8a10b6c07f2d4649567706c
Fernando <fom78a@gmail.com>
2022-04-25 03:06:53 -0300

    add GET organization

--- services/organizationServices.js
+++ services/organizationServices.js
@@ -1,5 +1,9 @@
 const { Organizations } = require('../models')
 module.exports = {
+  async findOrganization (id) {
+    const organization = await Organizations.findByPk(id)
+    return organization
+  },
   async postOrganization (name, imageUrl, phone, adress, welcomeText, urlFacebook, urlLinkedin, urlInstagram) {
     await Organizations.create({
       name, imageUrl, phone, adress, welcomeText, urlFacebook, urlLinkedin, urlInstagram
