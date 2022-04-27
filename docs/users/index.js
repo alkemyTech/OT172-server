2bd88e36c672de2e2fe419a45863384ec920329f
Fernando <fom78a@gmail.com>
2022-04-27 02:17:03 -0300

    add doc to users

--- docs/users/index.js
+++ docs/users/index.js
@@ -1,7 +1,15 @@
 const login = require('./postLogin')
+const register = require('./postRegister')
+const put = require('./put')
 
 module.exports = {
   '/auth/login': {
     ...login
+  },
+  '/auth/register': {
+    ...register
+  },
+  '/users/{id}': {
+    ...put
   }
 }
