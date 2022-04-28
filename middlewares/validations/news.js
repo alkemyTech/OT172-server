2d6af7c820865c6551db954abe0d12c2fc04acd8
KarlitosD <37454843+KarlitosD@users.noreply.github.com>
2022-04-28 03:59:42 -0300

    fix module import

--- middlewares/validations/news.js
+++ middlewares/validations/news.js
@@ -1,5 +1,5 @@
 // const Joi = require("joi")
-const { findNew } = require('../../services/newsService')
+const { findNew } = require('../../services/entriesService')
 
 const validateNew = schema => {
   return (req, res, next) => {
