c3830a49b710cd09af8f7a1fe447b0823d852369
KarlitosD <37454843+KarlitosD@users.noreply.github.com>
2022-04-28 15:37:51 -0300

    fix error handler middleware (#44)

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,14 +1,14 @@
-const { isHttpError } = require('http-errors')
+// const createError = require('http-errors')
+// const { isHttpError } = createError
 
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  const isCustomError = isHttpError(err)
-  res.status(isCustomError ? err.status : 500)
+  // const isCustomError = isHttpError(err)
+  res.status(err.status || 500)
   res.json({
     ok: false,
-    message: err.message
+    message: err.message || 'Internal server error'
   })
 }
