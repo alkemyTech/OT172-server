2fda68944dd6ece89174bf95500f07988258e835
Fernando Masino <49108644+fom78@users.noreply.github.com>
2022-04-29 04:23:21 -0300

    Usuarios 53 (#47)
    * add some data to tables
    
    * add service findUsers
    
    * add getAllUsers
    
    * add route get for users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,43 +1,55 @@
-const { findUser } = require('../services/userService')
+const { findUser, findUsers } = require('../services/userService')
 const { createAccessToken } = require('../auth/jwt')
 
-module.exports = {
-  async updateUser (req, res) {
-    try {
-      const user = await findUser(req.params.id)
+const updateUser = async (req, res) => {
+  try {
+    const user = await findUser(req.params.id)
 
-      if (user) {
-        Object.entries(req.body).forEach(item => {
-          const [key, value] = item
-          // user only update these fields
-          if (['firstName', 'lastName', 'email', 'image'].includes(key)) {
-            user[key] = value
-          }
-        })
-
-        if (req.user?.roleId === 1 && req.body.roleId) {
-          user.roleId = req.body.roleId // user admin an d self only can edit role.
+    if (user) {
+      Object.entries(req.body).forEach(item => {
+        const [key, value] = item
+        // user only update these fields
+        if (['firstName', 'lastName', 'email', 'image'].includes(key)) {
+          user[key] = value
         }
+      })
 
-        await user.save()
-        const { password, ...sentValues } = user.dataValues
-
-        // Create Token
-        const token = createAccessToken({
-          userId: sentValues.id,
-          email: sentValues.email
-        })
-        console.log(`User [${sentValues.email}] token was reset successful`)
-        res.status(200).json({
-          ok: true,
-          token,
-          user: sentValues
-        })
-      } else {
-        res.status(404).json({ ok: false, msg: 'User not found' })
+      if (req.user?.roleId === 1 && req.body.roleId) {
+        user.roleId = req.body.roleId // user admin an d self only can edit role.
       }
-    } catch (err) {
-      res.status(500).json({ error: err.message })
+
+      await user.save()
+      const { password, ...sentValues } = user.dataValues
+
+      // Create Token
+      const token = createAccessToken({
+        userId: sentValues.id,
+        email: sentValues.email
+      })
+      console.log(`User [${sentValues.email}] token was reset successful`)
+      res.status(200).json({
+        ok: true,
+        token,
+        user: sentValues
+      })
+    } else {
+      res.status(404).json({ ok: false, msg: 'User not found' })
     }
+  } catch (err) {
+    res.status(500).json({ error: err.message })
   }
+}
+
+const getAllUsers = async (req, res, next) => {
+  try {
+    const usersList = await findUsers()
+    res.status(200).json(usersList)
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  updateUser,
+  getAllUsers
 }
