a57523239cc69d61aa5d568020e4506dfd4fc78f
KarlitosD <37454843+KarlitosD@users.noreply.github.com>
2022-05-01 15:21:20 -0300

    add optional fields in postCreateContactSchema

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -3,8 +3,8 @@ const Joi = require('joi')
 const postCreateContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string(),
-  message: Joi.string()
+  phone: Joi.string().optional(),
+  message: Joi.string().optional()
 })
 
 module.exports = {
