420a19ebe9f24cc3cfb6b4545b50ed478da9eeac
KarlitosD <37454843+KarlitosD@users.noreply.github.com>
2022-04-30 22:02:24 -0300

    Endpoint POST contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { validateToken } = require('../middlewares/auth')
+const { validateToken, validateData } = require('../middlewares/auth')
 const { isAdmin } = require('../middlewares/checkRoles')
+const { postCreateContactSchema } = require('../schemas/contacts')
 
-const { getContacts } = require('../controllers/contactsController')
+const { getContacts, addContact } = require('../controllers/contactsController')
 
 router.get('/', [validateToken, isAdmin], getContacts)
+router.post('/', [validateToken, validateData(postCreateContactSchema)], addContact)
 
 module.exports = router
