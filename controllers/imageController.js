fad8cf0335539ed668e92b27ab89768068c4ee30
Fernando <fom78a@gmail.com>
2022-05-02 01:20:37 -0300

    temporaly image controller with name of path

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,30 +1,25 @@
-// const { postFile, getFile } = require('../services/amazonS3v2')
-const { postFile, getFile } = require('../services/amazonS3v3')
+const { postFile, getFile } = require('../services/amazonS3')
 
 module.exports = {
   async postImage (req, res) {
-    if (!req.file) {
-      return res.status(400).json({
-        error: 'Ha habido un error. Por favor, envíe una imagen en la petición.'
-      })
-    }
-
+    // Habilitar y probar al enviar archivos
+    // if (!req.file) {
+    //   return res.status(400).json({
+    //     error: 'Ha habido un error. Por favor, envíe una imagen en la petición.'
+    //   })
+    // }
+    const fileName = req.body.file
+    console.log('####', req.body.file)
     try {
-      const image = await postFile(req.file)
+      const image = await postFile(fileName)
 
-      console.log(`Image uploaded: ${req.file.originalname}`)
+      console.log(`Image uploaded: ${fileName}`)
       return res.status(201).json(image)
     } catch (err) {
       return res.status(500).json({ error: err.message })
     }
   },
   async getImage (req, res) {
-    if (!req.params.id) {
-      return res.status(400).json({
-        error: 'Por favor, provea el id del archivo'
-      })
-    }
-
     try {
       const image = await getFile(req.params.id)
 
