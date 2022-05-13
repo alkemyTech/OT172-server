904c143d384a4d0f896dd48fe39fb64123249a70
Nicolás A Borzone <nicolasborzone@any2cloud.com>
2022-05-13 20:04:30 -0300

    ok

--- services/sendgrid.js
+++ services/sendgrid.js
@@ -1,5 +1,5 @@
 const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey('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')
+sgMail.setApiKey(process.env.SEND_GRID)
 
 module.exports = sgMail;
