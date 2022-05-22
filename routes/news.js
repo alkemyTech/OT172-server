99c7c6b7202e9a633aa309522511390385374ccd
fom78 <fom78a@gmail.com>
2022-05-22 14:33:05 -0300

    now can get nes for all users

--- routes/news.js
+++ routes/news.js
@@ -9,8 +9,8 @@ const { validateData, validateExistenceNew } = require('../middlewares/dataValid
 const { getNewByIdSchema, createNewSchema } = require('../schemas/news')
 const { hasImage } = require('../middlewares/validations/image')
 
-router.get('/', validateToken, entriesController.getAllNews)
-router.get('/:id', [validateExistenceNew, validateData(getNewByIdSchema, 'params'), validateToken], entriesController.getNewById)
+router.get('/', entriesController.getAllNews)
+router.get('/:id', [validateExistenceNew, validateData(getNewByIdSchema, 'params')], entriesController.getNewById)
 router.delete('/:id', [validateToken, isAdmin, validateExistenceNew], entriesController.deleteNew)
 
 router.patch('/:id', [validateToken, validateData(createNewSchema, 'body'), validateExistenceNew, hasImage], entriesController.updateEntrie)
