8830f53d532e25c3ea9f3b41c440983fd2025bf4
Fernando <fom78a@gmail.com>
2022-05-25 22:34:04 -0300

    db pg

--- config/config.js
+++ config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql'
+    username: process.env.PG_DB_USER,
+    password: process.env.PG_DB_PASSWORD,
+    database: process.env.PG_DB_NAME,
+    host: process.env.PG_DB_HOST,
+    port: process.env.PG_DB_PORT,
+    dialect: 'postgres'
   },
   test: {
     username: process.env.DB_USER,
@@ -24,3 +24,27 @@ module.exports = {
     dialect: 'mysql'
   }
 }
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql'
+//   },
+//   test: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql'
+//   },
+//   production: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql'
+//   }
+// }
